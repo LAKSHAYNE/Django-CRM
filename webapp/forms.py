bd58f4ed5e5b6a9281d10b9cb7d8f3ee4ea03e58
Lakshay Sharma <56682042+LAKSHAYNE@users.noreply.github.com>
2024-01-25 23:21:09 +0530

    made a crm project in django

---
+++ webapp/forms.py
@@ -0,0 +1,30 @@
+from django.contrib.auth.forms import UserCreationForm ,AuthenticationForm
+from django.contrib.auth.models import User
+
+from . models import Record
+
+from django import forms
+from django.forms.widgets import PasswordInput, TextInput
+
+#register
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model=User
+        fields = ['username' ,'password1','password2']
+
+#login
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget=TextInput())
+    password = forms.CharField(widget=PasswordInput())
+
+class CreateRecordForm(forms.ModelForm):
+    class Meta:
+        model=Record
+        fields = ['first_name','last_name','email', 'phone','address','city','province','country']
+    
+
+class UpdateRecordForm(forms.ModelForm):
+    class Meta:
+        model=Record
+        fields = ['first_name','last_name','email', 'phone','address','city','province','country']
+ 
